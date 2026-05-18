c460647fa3885948acb00b51db608065bc8f0d60
Vgrz94 <stefano.togni@gmail.com>
2026-05-18 14:57:53 +0200

    Quartz sync: May 18, 2026, 2:57 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         title: "Hanalei Fill",
-        header: "Hanalei Fill",
+        header: "Roboto",
         body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
